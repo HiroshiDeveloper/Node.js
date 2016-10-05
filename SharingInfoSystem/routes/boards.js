619b33660b89f6f25f8a9ca0553ba0d8d2c2bddd
HiroshiDeveloper <19.hiroshi.13@gmail.com>
2016-10-04 18:32:35 -0700

    connect user info to each pages

---
+++ SharingInfoSystem/routes/boards.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var moment = require('moment');
+var connection = require('../../../mySqlConnection');
+
+router.get('/:board_id', function(req, res, next){
+	var boardId = req.params.board_id;
+	var getBoardQuery = 'SELECT * FROM boards WHERE board_id = ' + boardId;
+	
+	var getMessageQuery = 'SELECT M.message, ifnull(U.user_name, \'None\') AS user_name, DATE_FORMAT(M.created_at, \'%Y/%m/%d, %k:%i:%s\') AS created_at FROM messages M LEFT OUTER JOIN users U ON M.user_id = U.user_id WHERE M.board_id = ' + boardId + ' ORDER BY M.created_at ASC';
+	//var getMessageQuery = 'SELECT *, DATE_FORMAT(created_at, \'%Y / %m / %d : %k hrs %m mins %s secs\') AS created_at FROM messages WHERE board_id = ' + boardId;
+
+	connection.query(getBoardQuery, function(err, board){
+		connection.query(getMessageQuery, function(err, messages){
+			res.render('board', {
+				title : board[0].title,
+				board : board[0],
+				messageList : messages
+			});
+		});
+	});
+});
+
+router.post('/:board_id', function(req, res, next){
+	var message = req.body.message;
+	var boardId= req.params.board_id;
+	var userId = req.session.user_id? req.session.user_id: 0;
+	var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+	var query = 'INSERT INTO messages (message, board_id, user_id, created_at) VALUES ("' + message + '", ' + '"' + boardId + '", ' + '"' + userId + '", ' + '"' + createdAt + '")';
+	connection.query(query, function(err, rows){
+		res.redirect('/boards/' + boardId);
+	});
+});
+
+module.exports = router;
---
+++ SharingInfoSystem/routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var moment = require('moment');
+var connection = require('../../../mySqlConnection');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	
+	var query = 'SELECT B.board_id, B.user_id, B.title, ifnull(U.user_name, \'None\') AS user_name, DATE_FORMAT(B.created_at, \'%Y/%m/%d , %k:%m:%s\') AS created_at FROM boards B LEFT OUTER JOIN users U ON B.user_id = U.user_id ORDER BY B.created_at DESC';
+	
+	connection.query(query, function(err, rows){
+		console.log(rows);
+		res.render('index', {
+			title: 'Database API',
+			boardList: rows
+		});
+	});
+});
+
+
+router.post('/', function(req, res, next) {
+	var title = req.body.title;
+	var userId = req.session.user_id? req.session.user_id:0;
+
+	// date
+	var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+	
+	// mysql query
+	var query = 'INSERT INTO boards (user_id, title, created_at) VALUES ("' + userId + '", ' + '"' + title + '", ' + '"' + createdAt + '")';
+	console.log(query);
+	connection.query(query, function(err, rows){
+		res.redirect('/');
+	});
+	
+	console.log(title);
+	console.log(createdAt);
+});
+
+
+module.exports = router;
