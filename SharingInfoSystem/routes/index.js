0db0a9c5a446f03afd7809da689104b992f099ae
HiroshiDeveloper <19.hiroshi.13@gmail.com>
2016-10-04 21:55:41 -0700

    change the designs

--- SharingInfoSystem/routes/index.js
+++ SharingInfoSystem/routes/index.js
@@ -6,12 +6,17 @@ var connection = require('../../../mySqlConnection');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	
+	var userId = req.session.user_id? req.session.user_id:0;
+	if(!userId){
+		res.redirect('/login');
+	}
+	
 	var query = 'SELECT B.board_id, B.user_id, B.title, ifnull(U.user_name, \'None\') AS user_name, DATE_FORMAT(B.created_at, \'%Y/%m/%d , %k:%m:%s\') AS created_at FROM boards B LEFT OUTER JOIN users U ON B.user_id = U.user_id ORDER BY B.created_at DESC';
 	
 	connection.query(query, function(err, rows){
 		console.log(rows);
 		res.render('index', {
-			title: 'Database API',
+			title: 'Board',
 			boardList: rows
 		});
 	});
