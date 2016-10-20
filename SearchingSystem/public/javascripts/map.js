829768c26a545df62fb28a91aab164e09d8e70e1
HiroshiDeveloper <19.hiroshi.13@gmail.com>
2016-10-19 18:13:22 -0700

    customize a infoWindow

--- SearchingSystem/public/javascripts/map.js
+++ SearchingSystem/public/javascripts/map.js
@@ -1,7 +1,5 @@
-var message;
-var map;
-var marker;
-var myLatlng;
+var message, map, marker, myLatlng;
+var infowindow = new google.maps.InfoWindow();
  
 function start_func(){
 	get_location();
@@ -37,20 +35,18 @@ function initialize(x,y) {
      	// MapTypeId: HYBRID, ROADMAP, SATELLITE or TERRAIN
      	myLatlng = new google.maps.LatLng(x,y);
      	var mapOptions = {
-   		zoom: 15,
+   		zoom: 14,
+		enableHightAccuracy: true,
 	     	center: myLatlng,
 	     	mapTypeId: google.maps.MapTypeId.ROADMAP
     	 }
        
      	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-	createMarker(myLatlng, map, "Your position");
-	/*
 	var marker = new google.maps.Marker({
    		position: myLatlng,
 	     	map: map,
 		title:"Your position"
      	});
-	*/
 
      	get_area_name(myLatlng);
 	
@@ -64,8 +60,7 @@ function callback(results, status){
 	
 	if(status == google.maps.places.PlacesServiceStatus.OK){
 		for(var i=0; i<results.length; i++){
-			alert(results[i].geometry.location);
-			createMarker(results[i].geometry.location, map ,results[i].name);
+			createMarker(results[i]);
 		}
 	}
 }
@@ -93,11 +88,49 @@ function showMap(keyWord){
 }
 
 
-function createMarker(_position, _map, _title) {
+function createMarker(place) {
 	var marker = new google.maps.Marker({
-		position: _position,
-		map: _map,
-		title : _title
+		position: place.geometry.location,
+		map: map,
+		title : place.name
+	});
+
+
+	google.maps.event.addListener(marker, 'click', function() {
+		
+		var s ="";
+
+		/* icon + place name */
+		s+="<div class='ttl cf'>";
+		s+=(place.icon)?"<img width='32' height='32' src='"+place.icon+"' style='float:left;margin-right:5px;' />":"";
+		s+=(place.name)?"<b>"+place.name+"</b>":"?";
+		s+="</div>";
+		s+="<div class='detail'>";
+		
+		/* address */
+		if(place.formatted_address){
+			s+="<p>"+place.formatted_address+"</p>";
+		}
+
+		/* reviews */
+		if(place.rating){
+			    s+="<p>Reviews："+place.rating+"</p>";
+		}
+
+		/* pictures */
+		console.log("=== CHECK ===");
+		console.log(place.photos[0].getUrl);
+		if(place.photos && place.photos.length>=1){
+			    s+="<img src='"+place.photos[0].getUrl({"maxWidth":100,"maxHeight":100})+"'/>";
+		}
+
+
+
+
+
+		
+		infowindow.setContent("<div class='infowin'>"+s+"</div>");
+		infowindow.open(map, this);
 	});
 }
 
