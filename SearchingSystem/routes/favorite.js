e16b4e4d36295923569d18d5c5d22f6e9a69a928
HiroshiDeveloper <19.hiroshi.13@gmail.com>
2016-10-23 16:20:53 -0700

    customize favorite page

--- SearchingSystem/routes/favorite.js
+++ SearchingSystem/routes/favorite.js
@@ -7,9 +7,19 @@ var COL = 'favorite';
 
 router.get('/', function(req, res, next){
 	console.log("GO THROUGH FAVORITE PAGE");
-	res.render('favorite', {
-		title: 'FAVORITE'
+
+	collection(COL).find().toArray(function(err, data){
+    		console.log(data);
+		
+		//res.send(docs);
+		
+		res.render('favorite', {
+			title: 'FAVORITE',
+			jsondata: data
+		});
+
 	});
+
 });
 
 
